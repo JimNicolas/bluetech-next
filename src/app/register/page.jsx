bcfa2b8dd373a454144d20071a4d67e54529ab01
JimNicolas <jimeneznicolas2318@hotmail.com>
2023-05-27 03:03:34 -0400

    Se quitó el desorden donde los formularios de login y register eran tratados como el mismo componente donde solo se pasaban props para diferenciarlos

--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -1,11 +1,119 @@
-import Sign from '../components/common/Sign/Sign';
+'use client';
+import styles from './page.module.css';
+import { PageIcon, EyeIcon } from '../components/common/Icon';
+import Link from 'next/link';
+import Button from '../components/common/Button/Button';
+import { useState } from 'react';
+import { register } from '@/app/api/bluetechApi';
+import { useRouter } from 'next/navigation';
+import API_URL from '@/app/config';
+
 export const metadata = {
   title: 'Register',
 };
 export default function Register() {
+  const [showPassword, setShowPassword] = useState(false);
+  const [registeredFailed, setRegisteredFailed] = useState(false);
+  const router = useRouter();
+
+  const handleClickOnShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+  const registerHandle = async (event) => {
+    event.preventDefault();
+    const { registered } = await register(
+      `${API_URL}/register`,
+      event.target.username.value,
+      event.target.email.value,
+      event.target.password.value
+    );
+    registered ? router.push('/') : setRegisteredFailed(true);
+  };
+
   return (
     <>
-      <Sign type={'signUp'} namePage={'BlueTech'} title={'Welcome'} />
+      <main className={styles.container}>
+        <div className={styles.containerLeft}>
+          <PageIcon className={styles.icon} />
+          <h1 className={styles.leftTitle}>BlueTech</h1>
+        </div>
+        <div className={`${styles.containerRight} ${styles.containerSignUp}`}>
+          <h1 className={styles.titleRight}>Welcome</h1>
+          <form className={styles.form} onSubmit={registerHandle}>
+            {registeredFailed ? (
+              <>
+                <div className={styles.containerBubbleRegisteredFailed}>
+                  <h4>Este correo ya se encuentra reguistrado</h4>
+                </div>
+              </>
+            ) : (
+              ''
+            )}
+
+            <label className={styles.labelCredential} htmlFor='Username'>
+              Username
+            </label>
+            <input
+              className={styles.fullname}
+              name='username'
+              type='text'
+              placeholder='Enter Username'
+              id='username'
+            />
+            <label className={styles.labelCredential} htmlFor='email'>
+              Email
+            </label>
+            <input
+              className={styles.email}
+              name='Email'
+              type='email'
+              placeholder='Enter Email'
+              id='email'
+            />
+            <label className={styles.labelCredential} htmlFor='password'>
+              Password
+            </label>
+            <div className={styles.containerPassword}>
+              <input
+                className={styles.password}
+                name='Password'
+                type={showPassword ? 'text' : 'password'}
+                placeholder='Enter Password'
+                id='password'
+              />
+              <div className={styles.showPassword}>
+                <EyeIcon
+                  className={styles.iconEye}
+                  onClick={handleClickOnShowPassword}
+                />
+              </div>
+            </div>
+            <div className={styles.help}>
+              <div className={styles.containerCheckbox}>
+                <input
+                  className={styles.checkbox}
+                  name='checkbox'
+                  type='checkbox'
+                  id='checkbox'
+                />
+                <label htmlFor='checkbox'>I Aggre</label>
+              </div>
+            </div>
+
+            <Button
+              style={{ width: '100%' }}
+              type={'submit'}
+              value={'Sign Up'}
+            />
+          </form>
+          <span className={styles.lastSpan}>
+            Already a member? {''}
+            <Link className={styles.lastSpanLink} href={'/signIn'}>
+              Sign In
+            </Link>
+          </span>
+        </div>
+      </main>
     </>
   );
 }
--- src/app/signIn/page.jsx
+++ src/app/signIn/page.jsx
@@ -1,11 +1,106 @@
-import Sign from '../components/common/Sign/Sign.jsx';
+'use client';
+import styles from './page.module.css';
+import Link from 'next/link';
+import { PageIcon, EyeIcon } from '../components/common/Icon';
+import Button from '../components/common/Button/Button';
+import { useState } from 'react';
+import { loginAuthentication } from '@/app/api/bluetechApi';
+import { useRouter } from 'next/navigation';
+import API_URL from '@/app/config';
+
 export const metadata = {
   title: 'Login',
 };
 export default function SignIn() {
+  const [showPassword, setShowPassword] = useState(false);
+  const [loginFailed, setLoginFailed] = useState(false);
+  const router = useRouter();
+
+  const handleClickOnShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const loginHandle = async (event) => {
+    event.preventDefault();
+    const { authentication } = await loginAuthentication(
+      `${API_URL}/loginAuthentication`,
+      event.target.email.value,
+      event.target.password.value
+    );
+    authentication ? router.push('/') : setLoginFailed(true);
+  };
+
   return (
     <>
-      <Sign type={'signIn'} namePage={'BlueTech'} title={'Welcome Back'} />;
+      <main className={styles.container}>
+        <div className={styles.containerLeft}>
+          <PageIcon className={styles.icon} />
+          <h1 className={styles.leftTitle}>BlueTech</h1>
+        </div>
+        <div className={styles.containerRight}>
+          <h1 className={styles.titleRight}>Welcome Back</h1>
+          <form className={styles.form} onSubmit={loginHandle}>
+            <label className={styles.labelCredential} htmlFor='email'>
+              Email
+            </label>
+            <input
+              className={`${styles.email} ${
+                loginFailed ? styles.badCredentials : ''
+              }`}
+              name='Email'
+              type='email'
+              placeholder='Enter Email'
+              id='email'
+            />
+            <label className={styles.labelCredential} htmlFor='password'>
+              Password
+            </label>
+            <div className={styles.containerPassword}>
+              <input
+                className={`${styles.password} ${
+                  loginFailed ? styles.badCredentials : ''
+                }`}
+                name='Password'
+                type={showPassword ? 'text' : 'password'}
+                placeholder='Enter Password'
+                id='password'
+              />
+              <div className={styles.showPassword}>
+                <EyeIcon
+                  className={styles.iconEye}
+                  onClick={handleClickOnShowPassword}
+                />
+              </div>
+            </div>
+            <div className={styles.help}>
+              <div className={styles.containerCheckbox}>
+                <input
+                  className={styles.checkbox}
+                  name='checkbox'
+                  type='checkbox'
+                  id='checkbox'
+                />
+                <label htmlFor='checkbox'>Keep me logged in</label>
+              </div>
+              <Link className={styles.forgotPassword} href={'/forgot-password'}>
+                Forgot your password
+              </Link>
+            </div>
+            <Button
+              style={{ width: '100%' }}
+              type={'submit'}
+              value={'Sign In'}
+            />
+          </form>
+
+          <span className={styles.lastSpan}>
+            Don&apos;t have an account Sign up{' '}
+            <Link className={styles.lastSpanLink} href={'/register'}>
+              here
+            </Link>
+          </span>
+        </div>
+      </main>
     </>
   );
 }
