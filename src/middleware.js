d67dcdcfa5acc2fc6c1618297f2abc1db16cab44
JimNicolas <jimeneznicolas2318@hotmail.com>
2023-06-01 18:56:03 -0400

    config added in the middleware for protect route

--- src/middleware.js
+++ src/middleware.js
@@ -1,23 +1,27 @@
 import { NextResponse } from 'next/server';
 import { jwtVerify } from 'jose';
 export async function middleware(request) {
-  const cookie = request.cookies.get('userToken');
-  if (request.nextUrl.pathname.includes('/profile')) {
-    if (!cookie) {
-      return NextResponse.redirect(new URL('/signIn', request.url));
-    }
-    try {
-      const { payload } = await jwtVerify(
-        cookie.value,
-        new TextEncoder().encode('secretKey')
-      );
-      console.log('La cookie es: ', payload);
-      return NextResponse.next();
-    } catch (error) {
-      console.log('token no valido');
-      return NextResponse.redirect(new URL('/signIn', request.url));
-    }
-  }
+  const cookie = request.cookies.get('userToken'); // Recupera la cookie que hay en el navegador de nombre userToken (este nombre lo definimos en el backend)
 
-  return NextResponse.next();
+  // Si la ruta que se va a cargar contiene /profile // el if era de la siguiente forma pero para aplicar el
+  // config matcher para el bloque a todas la rutas se elimino el if: if (request.nextUrl.pathname.includes('/profile'))
+  if (!cookie) {
+    // Si no se encontro una cookie en el navegador
+    return NextResponse.redirect(new URL('/signIn', request.url)); // Redirige a la ruta /signin
+  }
+  try {
+    const { payload } = await jwtVerify(
+      // Verifica que el .value de la cookie corresponde a un formato de token valido
+      cookie.value,
+      new TextEncoder().encode('secretKey')
+    );
+    console.log('La cookie es: ', payload);
+    return NextResponse.next(); // Si verifica el token continua con el middleware
+  } catch (error) {
+    console.log('token no valido');
+    return NextResponse.redirect(new URL('/signIn', request.url)); // Si el token no es valido invita a volver a loguearse a la ruta signIn
+  }
 }
+export const config = {
+  matcher: ['/profile', '/products/:path*'], // Se aplicará el bloqueo a /profile y products/(cualquier subruta)
+};
